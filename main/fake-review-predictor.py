8b9c195e68c5c427506dbdf99c0e721b21d62457
Anupam Tripathi <tripathi.anupam1201@gmail.com>
2025-07-12 20:20:05 +0530

    Setup Basic sklearn libraries

---
+++ main/fake-review-predictor.py
@@ -0,0 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import joblib
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+
